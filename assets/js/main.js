77cb28dd1c023e947aad244be7f55d6b1b5ed238
Renan Pollo Benelli <111061315+Renan1Pollo@users.noreply.github.com>
2023-02-11 23:24:35 -0300

    commit att

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,25 +2,25 @@ const inputs = document.querySelectorAll('.input');
 const button = document.querySelector('.login-btn');
 
 const handleFocus = ({ target }) => {
-    const span = target.previousElementSibling;
-    span.classList.add('span-active');
-};
+  const span = target.previousElementSibling;
+  span.classList.add('span-active');
+}
 
 const handleFocusOut = ({ target }) => {
-    if (target.values = '') {
-        const span = target.previousElementSibling;
-        span.classList.remove('span-active');
-    }
-};
+  if (target.value === '') {
+    const span = target.previousElementSibling;
+    span.classList.remove('span-active');
+  }
+}
 
 const handleChange = () => {
-    const [username, password] = inputs;
+  const [username, password] = inputs;
 
-    if (username.value && password.value.length >= 8) {
-        button.removeAttribute('disabled');
-    } else {
-        button.setAttribute('disabled', '');
-    }
+  if (username.value && password.value.length >= 8) {
+    button.removeAttribute('disabled');
+  } else {
+    button.setAttribute('disabled', '');
+  }
 }
 
 inputs.forEach((input) => input.addEventListener('focus', handleFocus));
